d8c29071666bedee73d3fea0c51ceca53d8d527c
Selim TAVUKCUOGLU <selim@tavux.tech>
2024-02-16 11:34:47 +0100

    Remove docusaurus images

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,7 +6,7 @@ import Link from "@docusaurus/Link";
 const FeatureList = [
   {
     title: 'Empower Your Application with Carmentis',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    //Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
           Seamlessly integrate the Carmentis SDK into your application and become a pivotal operator in the network.
@@ -44,11 +44,11 @@ const FeatureList = [
   },*/
 ];
 
-function Feature({Svg, title, description, button_text, button_link}) {
+function Feature({title, description, button_text, button_link}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+          {/* <Svg className={styles.featureSvg} role="img" />*/}
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
