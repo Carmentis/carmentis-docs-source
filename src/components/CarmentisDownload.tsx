fc6dbe1a6e0928f082e8a99ee008eaf58662f4f1
Gaël Marcadet <gamarcadet@gmail.com>
2026-07-22 18:43:05 +0200

    fix: latest version of desk

--- src/components/CarmentisDownload.tsx
+++ src/components/CarmentisDownload.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 // https://github.com/Carmentis/carmentis-desk/releases/download/v1.2.2/carmentis-desk_1.2.2_.x64.dmg
 // https://github.com/Carmentis/carmentis-desk/releases/download/v1.2.2/carmentis-desk_1.2.2_x64.dmg
 
-const releasedVersions: string[]   = ["1.16.0"]
+zsh:1: command not found: q
 export function CarmentisDownload() {
   const content = releasedVersions.map((version, index) => {
     const release = {
